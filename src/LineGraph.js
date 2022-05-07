5a422ffada7019e34ad38a0c8bdd2fb67a69dd9d
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-05-06 21:54:29 -0300

    graph completed

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,45 +1,108 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
-function LineGraph() {
-    const [data, setData] = useState({})
+const options = {
+    legend: {
+        display: false,
+    },
+    elements: {
+        point: {
+            radius: 0,
+        },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0");
+            },
+        },
+    },
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                },
+            },
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    callback: function (value, index, values) {
+                        return numeral(value).format("0a");
+                    },
+                },
+            },
+        ],
+    },
+};
 
-    // https://disease.sh/v3/covid-19/historical/all?lastdays=120
-
-        useEffect(() => {
-            fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-            .then(response => response.json())
-            .then(data => {
-                //clever stuff here...
-                console.log(data);
-            })
-        }, [])
-
-        const buildChartData = (data, casesType) => {
-            const chartData = [];
-            let lastDataPoint;
-
-            data.casesType.forEach(date => {
-                if (lastDataPoint) {
-                    const newDataPoint = {
-                        x: date,
-                        y: data[casesType][date] - lastDataPoint
-                    }
-                    chartData.push(newDataPoint);
-                }
-                lastDataPoint = data[casesType][date];
-            })
-            return chartData;
+const buildChartData = (data, casesType) => {
+    let chartData = [];
+    let lastDataPoint;
+    for (let date in data.cases) {
+        if (lastDataPoint) {
+            let newDataPoint = {
+                x: date,
+                y: data[casesType][date] - lastDataPoint,
+            };
+            chartData.push(newDataPoint);
         }
+        lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+};
+
+function LineGraph({ casesType }) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    let chartData = buildChartData(data, casesType);
+                    setData(chartData);
+                    console.log(chartData);
+                    // buildChart(chartData);
+                });
+        };
+
+        fetchData();
+    }, [casesType]);
 
     return (
-    <div>
-        <h2>I am a graph</h2>
-    </div>
-  )
+        <div>
+            {data?.length > 0 && (
+                <Line
+                    data={{
+                        datasets: [
+                            {
+                                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                                borderColor: "#CC1034",
+                                data: data,
+                            },
+                        ],
+                    }}
+                    options={options}
+                />
+            )}
+        </div>
+    );
 }
 
-export default LineGraph
+export default LineGraph;
 
 //React chart js 2 - npm instal --save react-chartjs-2 or npm i react-chartjs-2
+// npm i numeral
